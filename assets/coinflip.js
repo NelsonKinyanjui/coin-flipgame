0f5351a3bea9c1744ece017ca8ee8c94e03e25ba
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-10-04 22:19:43 +0300

    debug code for efficient running

--- assets/coinflip.js
+++ assets/coinflip.js
@@ -1,7 +1,8 @@
 const randomNumber = Math.random();
-const result = randomNumber <= 0.5 ? 'Heads': 'Tails';
+const result = randomNumber < 0.5 ? 'Heads': 'Tails';
  
-let guess = 'Heads';
+const  guess = 'Heads';
+
 console.log(guess === 'Heads'? 'You win!' : 'You lose!');
 
 
